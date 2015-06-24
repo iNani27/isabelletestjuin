71220313e20198bd3b8f99d2bdd6afc48a6cd8d5
Stagiaire <Stagiaire@192.168.178.253>
2015-06-24 15:44:17 +0200

    unlink?

--- js/monjs.js
+++ js/monjs.js
@@ -1,6 +1,6 @@
 // fonction de confirmation pour supprimer un élément    
 function supprime(id) {
-    var question = confirm("Voulez-vous vraiment supprimer le proverbe dont l'id est " + id);
+    var question = confirm("Voulez-vous vraiment supprimer la photo dont l'id est " + id);
     if (question) { // si on répond ok
         // on redirige sur la page actuelle en donnant l'id à supprimer
         document.location.href = "?delete=" + id;
